3acc172ff760732f934457cf16f5c0e6932b4051
DgenR-8 <155558267+DgenR-8@users.noreply.github.com>
2025-12-11 01:38:48 +0000

    Ecommerce web application source files
    Original ecommerce web application source files

---
+++ src/main/java/com/devjdt/ecom/enums/UserRole.java
@@ -0,0 +1,8 @@
+package com.devjdt.ecom.enums;
+
+public enum UserRole {
+
+    ADMIN,
+
+    CUSTOMER
+}
---
+++ src/test/java/com/devjdt/ecom/EcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjdt.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
